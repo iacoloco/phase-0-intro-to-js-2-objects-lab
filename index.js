3d57f43bec46c8b0c081fc0813056651070e0f8e
Iachini <iachiniarmndo@gmail.com>
2022-12-28 18:39:49 +0000

    commit tree 4981e3a3c8f0cdabe172eb4a1af27e57ff18e0cf

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+ const employee = {
+    name:"",
+    streetAddress:"",
+ };
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    return {
+        ...employee,
+        [key]:value,
+    }
+};
+ 
+const newEmployee = updateEmployeeWithKeyAndValue(employee,"streetAddress","11 Broadway");
+//
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value;
+    return employee ;
+}
+    function deleteFromEmployeeByKey(employee, key){
+    var clone = {...employee};
+    delete clone[key]
+    return clone
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+}
+
+
+
+
+
+
+
+
+
+
+
+
